646b3cc829cee26903ac36832ecb9dd2322ee5cc
SabrinaZ99 <12Sabrina21@gmail.com>
2024-08-20 15:20:30 -0400

    eod push

--- tstng/082024/CsharpProjects/TestProject/Program.cs
+++ tstng/082024/CsharpProjects/TestProject/Program.cs
@@ -1,6 +1,63 @@
-﻿Console.WriteLine("a" == "a");
-Console.WriteLine("a" == "A");
-Console.WriteLine(1 == 2);
+﻿///////
+///  VAR SCOPE PRACTICE
+///  8-20-24
+///////////////////////////////////////////////////////////////////
 
-string myValue = "a";
-Console.WriteLine(myValue == "a");
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         bool flag = true;
+//         int value = 55;
+//         if (flag)
+//         {
+//             value = 10;
+//             Console.WriteLine($"Inside the code block: {value}");
+//         }
+//         Console.WriteLine($"Outside the code block: {value}");
+
+//     }
+// }
+
+
+///////////////////////////////////////////////////////////////
+/// IF-ELSE syntax practice
+
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         bool flag = true;
+//         if (flag)
+//             Console.WriteLine(flag);
+//         else
+//             Console.WriteLine("u just lost the game");
+//     }
+// }
+//////////////////////////////////////////////////////////////////
+/// VAR SCOPE CHALLENGE ACTIVITY
+
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         int[] numbers = { 4, 8, 15, 16, 23, 42 };
+//         bool found = false;
+//         int total = 0;
+//         foreach (int number in numbers)
+//         {
+//             total += number;
+//             if (number == 42)
+//                 found = true;
+//         }
+
+//         if (found)
+//             Console.WriteLine("Set contains 42");
+
+//         Console.WriteLine($"Total: {total}");
+//     }
+// }
+
+
+/////////////////////////////////////////////////////////////
+/// Code Flow branching w/switch-case construct
--- tstng/082024/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ tstng/082024/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da11d3358364ac9fe6814716a8f94c64fcb0cd06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a40708b5fb4f8b1dd6004aece3af4e79504b253c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
