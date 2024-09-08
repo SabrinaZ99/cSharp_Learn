c1480e776d97d7cff9614a423bc0d34eaf4af1f3
SabrinaZ99 <12Sabrina21@gmail.com>
2024-09-08 16:24:46 -0400

    add logic to csharp console apps end

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         for (int i = 1; i <= 100; i++)
+//         {
+//             if (i % 15 == 0) Console.WriteLine($"{i}-FizzBuzz");
+//             else if (i % 5 == 0) Console.WriteLine($"{i}-Buzz");
+//             else if (i % 3 == 0) Console.WriteLine($"{i}-Fizz");
+//             else Console.WriteLine(i);
+//         }
+//     }
+// }
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+        int current = 0;
+
+        do
+        {
+            current = random.Next(1, 11);
+            Console.WriteLine(current);
+        }
+        while (current != 7);
+    }
+}
